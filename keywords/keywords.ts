42ab6d600644246f652a7fcd5b4fa0b53d22a5f7
Awan-Haidir <cloudzzlearning@gmail.com>
2026-05-25 22:51:06 +0700

    add keyword judi

--- keywords/keywords.ts
+++ keywords/keywords.ts
@@ -2,7 +2,8 @@ export const KEYWORD_JUDI = [
     'slot', 'judi', 'togel', 'casino', 'taruhan', 'betting',
     'gacor', 'maxwin', 'jackpot', 'scatter', 'rtp', 'spin',
     'pragmatic', 'pg soft', 'habanero',
-    'jp', 'hoki', 'madu', 'cuan', 'bet',
-    'gacor99', 'maxwin88', 'slot88', 'slot99', 'dewapetir'
+    'jp', 'hoki', 'madu', 'cuan', 'bet', 'master',
+    'gacor99', 'maxwin88', 'slot88', 'slot99', 'dewapetir88',
+    'zeus88', 'zeus99', 'master88', 'master99'
 ];
 
